926161f3bf6436f346fd2ba4d24f8007509c0158
Michal <osboxes@localhost.localdomain>
2020-05-06 08:43:36 -0400

    Adding server, not ready, not tested

---
+++ game.c
@@ -0,0 +1,11 @@
+#include "game.h"
+
+void init_player(player_t* player)
+{
+    return;
+}
+
+void remove_player(player_t* player)
+{
+    return;
+}
---
+++ game.h
@@ -0,0 +1,29 @@
+typedef struct player_t player_t;
+typedef struct point_t point_t;
+typedef enum move_t move_t;
+
+//just point
+struct point_t
+{
+    int x,y;
+};
+
+struct player_t
+{
+    point_t cords;
+    point_t vel;
+
+};
+
+//DEPEND ON CLIENT PREFERENCES
+enum move_t
+{
+    UP,
+    LEFT,
+    RIGHT,
+    END,
+};
+
+void init_player(player_t* player);
+
+void remove_player(player_t* player);
