d55e3c9dd1db7a56147f8e7df392818518c56861
Michal <osboxes@localhost.localdomain>
2020-05-06 10:06:19 -0400

    Adding server, probably ready, but it can have some bugs

---
+++ game.c
@@ -0,0 +1,16 @@
+#include "game.h"
+
+void init_player(player_t* player)
+{
+    return;
+}
+
+void remove_player(player_t* player)
+{
+    return;
+}
+
+void make_move(player_t* player, move_t move)
+{
+    return;
+}
