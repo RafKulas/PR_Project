8cf786b094b75ca61b30604dc44002355ea32d41
RafKulas <leormas1@gmail.com>
2020-05-15 21:47:32 +0200

    Completed v0.1.4 removed to many debug_printf's

--- connection.c
+++ connection.c
@@ -65,7 +65,6 @@ void recv_structure(int sock, game_object* pdata)
 	int buffer[BUFOR_SIZE_INT];
 	recv(sock, (char *)buffer, BUFOR_SIZE_CHAR, NO_FLAGS);
 
-	debug_printf(pdata);
 	pdata->obstacles_number = buffer[BUFOR_START];
 
 	for (int i = 0; i < pdata->obstacles_number; ++i)
