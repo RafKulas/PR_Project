29187490012c6dc3ba4e1027a3a1edeb41e8afdd
Rafał Kulik <50609857+RafKulas@users.noreply.github.com>
2020-05-17 18:10:48 +0200

    Let the God be with us

--- connection.c
+++ connection.c
@@ -30,13 +30,17 @@ void send_structure(int sock, game_object_t* pdata)
 
     buffer[BUFOR_START] = pdata->players_index;
 
+    int a,b;
     for (int i = 0; i < pdata->players_index; ++i)
     {
-        *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0] = pdata->players[i].player_rect.cords.x;
-        *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1] = pdata->players[i].player_rect.cords.y;
-        *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2] = pdata->players[i].player_rect.width;
-        *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 3] = pdata->players[i].player_rect.height;
-        *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4] = pdata->players[i].vel;
+        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0] = pdata->players[i].player_rect.cords.x/100;
+        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0 + 1] = pdata->players[i].player_rect.cords.x%100;
+        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1] = pdata->players[i].player_rect.cords.y/100;
+        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1 + 1] = pdata->players[i].player_rect.cords.y%100;
+
+        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2] = pdata->players[i].player_rect.width;
+        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 3] = pdata->players[i].player_rect.height;
+        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4] = pdata->players[i].vel;
     }
 
     debug_printf(pdata);
@@ -73,14 +77,20 @@ void recv_structure(int sock, game_object_t* pdata)
     recv(sock, (char *)buffer, BUFOR_SIZE_CHAR, NO_FLAGS);
 
     pdata->players_index = buffer[BUFOR_START];
-
+    int a,b;
     for (int i = 0; i < pdata->players_index; ++i)
     {
-        pdata->players[i].player_rect.cords.x = *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0];
-        pdata->players[i].player_rect.cords.y = *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1];
-        pdata->players[i].player_rect.width   = *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2];
-        pdata->players[i].player_rect.height  = *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 3];
-        pdata->players[i].vel				  = *(int*)buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4];
+        //pdata->players[i].player_rect.cords.x = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0];
+        a = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0];
+        b = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0 + 1];
+        pdata->players[i].player_rect.cords.x = a*100+b;
+        //pdata->players[i].player_rect.cords.y = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1];
+        a = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1];
+        b = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1 + 1];
+        pdata->players[i].player_rect.cords.y = a*100+b;
+        pdata->players[i].player_rect.width   = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2];
+        pdata->players[i].player_rect.height  = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 3];
+        pdata->players[i].vel				  = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4];
     }
 
     debug_printf(pdata);
