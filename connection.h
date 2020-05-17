c80846dce9233e8bc504f199eb9d612deadf51e9
JJK <jan.jakub.korbel@gmail.com>
2020-05-17 13:54:45 +0200

    some optimalization changes

--- connection.h
+++ connection.h
@@ -4,11 +4,17 @@
 
 #define NO_FLAGS 0
 
+#define CHAR_SIZE sizeof(char)
 #define INT_SIZE sizeof(int)
 #define RECT_SIZE sizeof(rect_t)
 #define PLAYER_SIZE sizeof(player_t)
 
-#define BUFOR_SIZE_INT 1024
-#define BUFOR_SIZE_CHAR BUFOR_SIZE_INT * INT_SIZE
+#define BUFOR_SIZE_CHAR 4096
 
 #define BUFOR_START 0
+
+void send_init_structure(int sock, game_object_t* pdata);
+void send_structure(int sock, game_object_t* pdata);
+void recv_init_structure(int sock, game_object_t* pdata);
+void recv_structure(int sock, game_object_t* pdata);
+void debug_printf(game_object_t* data);
