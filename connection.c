bbb1f186c28a82736adf1aaff8b76f7367e4e133
RafKulas <leormas1@gmail.com>
2020-05-17 19:00:29 +0200

    Completed v0.1.8 Little left

--- connection.c
+++ connection.c
@@ -30,12 +30,13 @@ void send_structure(int sock, game_object_t* pdata)
 
     buffer[BUFOR_START] = pdata->players_index;
 
+    int a,b;
     for (int i = 0; i < pdata->players_index; ++i)
     {
-        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0] = pdata->players[i].player_rect.cords.x;
-        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1] = pdata->players[i].player_rect.cords.y;
-        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2] = pdata->players[i].player_rect.width;
-        buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 3] = pdata->players[i].player_rect.height;
+        *(int*)(buffer + PLAYER_SIZE * i + 1 + INT_SIZE * 0) = pdata->players[i].player_rect.cords.x;
+        *(int*)(buffer + PLAYER_SIZE * i + 1 + INT_SIZE * 1) = pdata->players[i].player_rect.cords.y;
+        *(int*)(buffer + PLAYER_SIZE * i + 1 + INT_SIZE * 2) = pdata->players[i].player_rect.width;
+        *(int*)(buffer + PLAYER_SIZE * i + 1 + INT_SIZE * 3) = pdata->players[i].player_rect.height;
         buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4] = pdata->players[i].vel;
     }
 
@@ -56,13 +57,13 @@ void recv_init_structure(int sock, game_object_t* pdata)
     {
         pdata->obstacles[i].cords.x = buffer[RECT_SIZE * i + 1];
         pdata->obstacles[i].cords.y = buffer[RECT_SIZE * i + 2];
-        pdata->obstacles[i].width = buffer[RECT_SIZE * i + 3];
-        pdata->obstacles[i].height = buffer[RECT_SIZE * i + 4];
+        pdata->obstacles[i].width   = buffer[RECT_SIZE * i + 3];
+        pdata->obstacles[i].height  = buffer[RECT_SIZE * i + 4];
     }
 
     pdata->players_index = buffer[1 + buffer[0] * RECT_SIZE];
 
-    pdata->board_width = buffer[1 + buffer[0] * RECT_SIZE + 1 + buffer[1 + buffer[0] * RECT_SIZE] * PLAYER_SIZE];
+    pdata->board_width  = buffer[1 + buffer[0] * RECT_SIZE + 1 + buffer[1 + buffer[0] * RECT_SIZE] * PLAYER_SIZE];
     pdata->board_height = buffer[1 + buffer[0] * RECT_SIZE + 1 + buffer[1 + buffer[0] * RECT_SIZE] * PLAYER_SIZE + 1];
     debug_printf(pdata);
 }
@@ -73,14 +74,14 @@ void recv_structure(int sock, game_object_t* pdata)
     recv(sock, (char *)buffer, BUFOR_SIZE_CHAR, NO_FLAGS);
 
     pdata->players_index = buffer[BUFOR_START];
-
+    int a,b;
     for (int i = 0; i < pdata->players_index; ++i)
     {
-        pdata->players[i].player_rect.cords.x = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0];
-        pdata->players[i].player_rect.cords.y = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1];
-        pdata->players[i].player_rect.width   = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2];
-        pdata->players[i].player_rect.height  = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 3];
-        pdata->players[i].vel				  = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4];
+        pdata->players[i].player_rect.cords.x = *(int*)(buffer + PLAYER_SIZE * i + 1 + INT_SIZE * 0);
+        pdata->players[i].player_rect.cords.y = *(int*)(buffer + PLAYER_SIZE * i + 1 + INT_SIZE * 1);
+        pdata->players[i].player_rect.width = *(int*)(buffer + PLAYER_SIZE * i + 1 + INT_SIZE * 2);
+        pdata->players[i].player_rect.height = *(int*)(buffer + PLAYER_SIZE * i + 1 + INT_SIZE * 3);
+        pdata->players[i].vel = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4];
     }
 
     debug_printf(pdata);
