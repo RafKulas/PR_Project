a809b7a4953a1514e07294cb53c59f689b1749cc
JJK <jan.jakub.korbel@gmail.com>
2020-05-18 07:46:25 +0200

    structure changes

--- connection.c
+++ connection.c
@@ -6,22 +6,33 @@ void send_init_structure(int sock, game_object_t* pdata)
 {
 	char buffer[BUFOR_SIZE_CHAR];
 
-	buffer[BUFOR_START] = pdata->obstacles_number;
+	buffer[BUFOR_START] = pdata->ID;
+
+	buffer[1] = pdata->obstacles_number;
 
 	for (int i = 0; i < pdata->obstacles_number; ++i)
 	{
-		buffer[RECT_SIZE * i + 1] = pdata->obstacles[i].cords.x;
-		buffer[RECT_SIZE * i + 2] = pdata->obstacles[i].cords.y;
-		buffer[RECT_SIZE * i + 3] = pdata->obstacles[i].width;
-		buffer[RECT_SIZE * i + 4] = pdata->obstacles[i].height;
+		buffer[RECT_SIZE * i + 2] = pdata->obstacles[i].cords.x;
+		buffer[RECT_SIZE * i + 3] = pdata->obstacles[i].cords.y;
+		buffer[RECT_SIZE * i + 4] = pdata->obstacles[i].width;
+		buffer[RECT_SIZE * i + 5] = pdata->obstacles[i].height;
 	}
 
-	buffer[1 + buffer[0] * RECT_SIZE] = 0;
+	buffer[1 + 1 + RECT_SIZE * buffer[1]] = pdata->speed_spots_number;
 
-	buffer[1 + buffer[0] * RECT_SIZE + 1 + buffer[1 + buffer[0] * RECT_SIZE] * PLAYER_SIZE] = pdata->board_width;
-	buffer[1 + buffer[0] * RECT_SIZE + 1 + buffer[1 + buffer[0] * RECT_SIZE] * PLAYER_SIZE + 1] = pdata->board_height;
-	debug_printf(pdata);
-	send(sock, (char *)(buffer), (1 + buffer[0] * RECT_SIZE + 1 + buffer[1 + buffer[0] * RECT_SIZE] * PLAYER_SIZE + 1 + 1) * CHAR_SIZE, NO_FLAGS);
+	for (int i = 0; i < pdata->speed_spots_number; ++i)
+	{
+		buffer[RECT_SIZE * i + 3 + RECT_SIZE * buffer[1]] = pdata->speed_spots[i].cords.x;
+		buffer[RECT_SIZE * i + 4 + RECT_SIZE * buffer[1]] = pdata->speed_spots[i].cords.y;
+		buffer[RECT_SIZE * i + 5 + RECT_SIZE * buffer[1]] = pdata->speed_spots[i].width;
+		buffer[RECT_SIZE * i + 6 + RECT_SIZE * buffer[1]] = pdata->speed_spots[i].height;
+	}
+
+	buffer[1 + 1 + RECT_SIZE * buffer[1] + 1 + RECT_SIZE * buffer[1 + 1 + RECT_SIZE * buffer[1]]] = 0;
+
+	buffer[1 + 1 + RECT_SIZE * buffer[1] + 1 + RECT_SIZE * buffer[1 + 1 + RECT_SIZE * buffer[1]] + 1] = pdata->board_width;
+	buffer[1 + 1 + RECT_SIZE * buffer[1] + 1 + RECT_SIZE * buffer[1 + 1 + RECT_SIZE * buffer[1]] + 1 + 1] = pdata->board_height;
+	send(sock, (char *)(buffer), (1 + 1 + RECT_SIZE * buffer[1] + 1 + RECT_SIZE * buffer[1 + 1 + RECT_SIZE * buffer[1]] + 1 + 1 + 1) * CHAR_SIZE, NO_FLAGS);
 }
 
 void send_structure(int sock, game_object_t* pdata)
@@ -32,14 +43,12 @@ void send_structure(int sock, game_object_t* pdata)
 
 	for (int i = 0; i < pdata->players_index; ++i)
 	{
-		buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0] = pdata->players[i].player_rect.cords.x;
-		buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1] = pdata->players[i].player_rect.cords.y;
-		buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2] = pdata->players[i].player_rect.width;
-		buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 3] = pdata->players[i].player_rect.height;
-		buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4] = pdata->players[i].vel;
+		*(int*)&(buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0]) = pdata->players[i].player_rect.cords.x;
+		*(int*)&(buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1]) = pdata->players[i].player_rect.cords.y;
+		buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2] = pdata->players[i].id;
+		buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2 + 1] = pdata->players[i].vel;
 	}
 
-	debug_printf(pdata);
 	send(sock, (char *)(buffer), (1 + buffer[0] * PLAYER_SIZE) * CHAR_SIZE, NO_FLAGS);
 }
 
@@ -48,23 +57,33 @@ void recv_init_structure(int sock, game_object_t* pdata)
 {
 	char buffer[BUFOR_SIZE_CHAR];
 	recv(sock, (char *)buffer, BUFOR_SIZE_CHAR, NO_FLAGS);
-	
 
-	pdata->obstacles_number = buffer[BUFOR_START];
+	pdata->ID = buffer[BUFOR_START];
+
+	pdata->obstacles_number = buffer[1];
 
 	for (int i = 0; i < pdata->obstacles_number; ++i)
 	{
-		pdata->obstacles[i].cords.x = buffer[RECT_SIZE * i + 1];
-		pdata->obstacles[i].cords.y = buffer[RECT_SIZE * i + 2];
-		pdata->obstacles[i].width = buffer[RECT_SIZE * i + 3];
-		pdata->obstacles[i].height = buffer[RECT_SIZE * i + 4];
+		pdata->obstacles[i].cords.x = buffer[RECT_SIZE * i + 2];
+		pdata->obstacles[i].cords.y = buffer[RECT_SIZE * i + 3];
+		pdata->obstacles[i].width = buffer[RECT_SIZE * i + 4];
+		pdata->obstacles[i].height = buffer[RECT_SIZE * i + 5];
 	}
 
-	pdata->players_index = buffer[1 + buffer[0] * RECT_SIZE];
+	pdata->speed_spots_number = buffer[1 + 1 + RECT_SIZE * buffer[1]];
 
-	pdata->board_width = buffer[1 + buffer[0] * RECT_SIZE + 1 + buffer[1 + buffer[0] * RECT_SIZE] * PLAYER_SIZE];
-	pdata->board_height = buffer[1 + buffer[0] * RECT_SIZE + 1 + buffer[1 + buffer[0] * RECT_SIZE] * PLAYER_SIZE + 1];
-	debug_printf(pdata);
+	for (int i = 0; i < pdata->speed_spots_number; ++i)
+	{
+		pdata->speed_spots[i].cords.x = buffer[RECT_SIZE * i + 3 + RECT_SIZE * buffer[1]];
+		pdata->speed_spots[i].cords.y = buffer[RECT_SIZE * i + 4 + RECT_SIZE * buffer[1]];
+		pdata->speed_spots[i].width = buffer[RECT_SIZE * i + 5 + RECT_SIZE * buffer[1]];
+		pdata->speed_spots[i].height = buffer[RECT_SIZE * i + 6 + RECT_SIZE * buffer[1]];
+	}
+
+	pdata->players_index = buffer[1 + 1 + RECT_SIZE * buffer[1] + 1 + RECT_SIZE * buffer[1 + 1 + RECT_SIZE * buffer[1]]];
+
+	pdata->board_width = buffer[1 + 1 + RECT_SIZE * buffer[1] + 1 + RECT_SIZE * buffer[1 + 1 + RECT_SIZE * buffer[1]] + 1];
+	pdata->board_height = buffer[1 + 1 + RECT_SIZE * buffer[1] + 1 + RECT_SIZE * buffer[1 + 1 + RECT_SIZE * buffer[1]] + 1 + 1];
 }
 
 void recv_structure(int sock, game_object_t* pdata)
@@ -76,14 +95,12 @@ void recv_structure(int sock, game_object_t* pdata)
 
 	for (int i = 0; i < pdata->players_index; ++i)
 	{
-		pdata->players[i].player_rect.cords.x = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0];
-		pdata->players[i].player_rect.cords.y = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1];
-		pdata->players[i].player_rect.width   = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2];
-		pdata->players[i].player_rect.height  = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 3];
-		pdata->players[i].vel				  = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 4];
+		pdata->players[i].player_rect.cords.x = *(int*)&buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 0];
+		pdata->players[i].player_rect.cords.y = *(int*)&buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 1];
+		pdata->players[i].id = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2];
+		pdata->players[i].vel = buffer[PLAYER_SIZE * i + 1 + INT_SIZE * 2 + 1];
 	}
 
-	debug_printf(pdata);
 }
 
 void debug_printf(game_object_t* data)
@@ -97,16 +114,24 @@ void debug_printf(game_object_t* data)
 		printf("data.obstacles[i].width = %d\n", data->obstacles[i].width);
 		printf("data.obstacles[i].height = %d\n", data->obstacles[i].height);
 	}
+	printf("data.obstacles_number = %d\n", data->obstacles_number);
+	for (int i = 0; i < data->speed_spots_number; ++i)
+	{
+		printf("i = %d\n", i);
+		printf("data.speed_spots[i].cords.x = %d\n", data->speed_spots[i].cords.x);
+		printf("data.speed_spots[i].cords.y = %d\n", data->speed_spots[i].cords.y);
+		printf("data.speed_spots[i].width = %d\n", data->speed_spots[i].width);
+		printf("data.speed_spots[i].height = %d\n", data->speed_spots[i].height);
+	}
+	printf("data.board_width = %d\n", data->board_width);
+	printf("data.board_height = %d\n", data->board_height);
 	printf("data.players_index =  %d\n", data->players_index);
 	for (int i = 0; i < data->players_index; ++i)
 	{
 		printf("i = %d\n", i);
 		printf("data.players[i].player_rect.cords.x = %d\n", data->players[i].player_rect.cords.x);
 		printf("data.players[i].player_rect.cords.y = %d\n", data->players[i].player_rect.cords.y);
-		printf("data.players[i].player_rect.width = %d\n", data->players[i].player_rect.width);
-		printf("data.players[i].player_rect.height = %d\n", data->players[i].player_rect.height);
 		printf("data.players[i].vel = %d\n", data->players[i].vel);
+		printf("data.players[i].id = %d\n", data->players[i].id);
 	}
-	printf("data.board_width = %d\n", data->board_width);
-	printf("data.board_height = %d\n", data->board_height);
 }
--- game_structure.h
+++ game_structure.h
@@ -7,18 +7,17 @@ typedef struct rect_t rect_t;
 typedef struct pair_t_player pair_t_player;
 typedef struct rect_t_player rect_t_player;
 typedef enum move_t move_t;
-typedef enum bool_t bool_t;
 typedef struct game_object_t game_object_t;
 
 struct pair_t
 {
-    char x,y;
+	char x, y;
 };
 
 struct rect_t
 {
-    pair_t cords; //cords of left upper corner
-    char width, height; //not point to make things simple
+	pair_t cords; //cords of left upper corner
+	char width, height; //not point to make things simple
 };
 
 struct pair_t_player
@@ -29,33 +28,37 @@ struct pair_t_player
 struct rect_t_player
 {
 	pair_t_player cords; //cords of left upper corner
-	int width, height; //not point to make things simple
+	int size, game_result; //not point to make things simple
 };
 
 struct player_t
 {
 	rect_t_player player_rect;
-    char vel;
+	char vel;
+	char id;
 };
 
 //DEPEND ON CLIENT PREFERENCES
 enum move_t
 {
-    UP = 1,
-    DOWN = 2,
-    LEFT = 3,
-    RIGHT = 4,
-    END = 5,
-    BEYOND = 6,
+	UP = 1,
+	DOWN = 2,
+	LEFT = 3,
+	RIGHT = 4,
+	END = 5,
+	BEYOND = 6,
 };
 
 struct game_object_t
 {
-    char obstacles_number;
-    rect_t* obstacles;
-    char players_index;
-    player_t* players;
-    char board_width,board_height; //not point to make things simple
+	char ID;
+	char obstacles_number;
+	rect_t* obstacles;
+	char speed_spots_number;
+	rect_t* speed_spots;
+	char board_width, board_height; //not point to make things simple
+	char players_index;
+	player_t* players;
 };
 
 #endif //MAIN_GAME_STRUCTURE_H
